e95bdea28f39755f56b9e3485306f15e1ec3a92d
Dagmbisrat <dagmbisrat7@gmail.com>
2024-09-25 00:46:36 -0400

    changed to work for oracel

--- lab3/CreateDB.sql
+++ lab3/CreateDB.sql
@@ -1,19 +1,12 @@
--- Create an ENUM type for application status
-CREATE TYPE applicationStatus AS ENUM (
-    'pending',
-    'reviewed',
-    'accepted',
-    'rejected'
-);
-
 -- Create the Application table
 CREATE TABLE Application (
     jobseeker_id INT,
     job_id INT,
-    resume TEXT NOT NULL,
-    coverletter TEXT,
-    status applicationStatus,
+    resume CLOB NOT NULL,  
+    coverletter CLOB,     
+    status VARCHAR2(20),
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT status_check CHECK (status IN ('pending', 'reviewed', 'accepted', 'rejected')),
     PRIMARY KEY (jobseeker_id, job_id),
     FOREIGN KEY (jobseeker_id) REFERENCES JobSeeker(jobseeker_id),
     FOREIGN KEY (job_id) REFERENCES Job(job_id)
@@ -21,22 +14,23 @@ CREATE TABLE Application (
 
 -- Create a table to store unique skills
 CREATE TABLE Skills (
-    skill_name VARCHAR(255) PRIMARY KEY
+    skill_name VARCHAR2(255) PRIMARY KEY
 );
 
 
 -- Create a JobSeekerSkills table
 CREATE TABLE JobSeekerSkills (
-    skill_name VARCHAR(255),
+    skill_name VARCHAR2(255),  
     job_seeker_id INT,
     PRIMARY KEY (skill_name, job_seeker_id),
     FOREIGN KEY (skill_name) REFERENCES Skills(skill_name),
     FOREIGN KEY (job_seeker_id) REFERENCES JobSeeker(job_seeker_id)
 );
 
+
 -- Create the JobSkills table
 CREATE TABLE JobSkills (
-    skill_name VARCHAR(255),
+    skill_name VARCHAR2(255),  
     job_id INT,
     PRIMARY KEY (skill_name, job_id),
     FOREIGN KEY (skill_name) REFERENCES Skills(skill_name),
