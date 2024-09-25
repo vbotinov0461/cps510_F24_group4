051c906abcadbae942f7e2b83effb7079a400c67
Shivesh-Mahato <128956579+Shivesh-Mahato@users.noreply.github.com>
2024-09-25 10:33:33 -0400

    Update SimpleQueries.sql

--- lab3/SimpleQueries.sql
+++ lab3/SimpleQueries.sql
@@ -1,7 +1,7 @@
 -- Retrieve distinct jobseeker IDs and order them 
-SELECT DISTINCT jobseeker_id
+SELECT DISTINCT job_seeker_id
 FROM Application
-ORDER BY jobseeker_id ASC;
+ORDER BY job_seeker_id ASC;
 
 -- Count the number of distinct skills required for each job by grouping by job_id
 SELECT job_id, COUNT(DISTINCT skill_name) AS distinct_skill_count
@@ -12,3 +12,28 @@ GROUP BY job_id;
 SELECT DISTINCT skill_name
 FROM Skills
 ORDER BY skill_name ASC;
+
+-- Retrieve all jobseekers
+SELECT *
+FROM jobseeker;
+
+
+/* Retrieve the name of the jobseeker with ID number4 */
+SELECT Jobseeker_Name
+FROM jobseeker
+WHERE Job_seeker_Id = 4;
+
+/* Select job postings where the name of the employer is Apple */
+SELECT *
+FROM job
+WHERE Employer_Name = 'Apple';
+
+/* Select job seekers who have a Master's in Computer Science from TMU*/
+SELECT *
+FROM jobseeker
+WHERE education_id = 2;
+
+-- 
+SELECT j.Jobseeker_Name
+FROM education e, jobseeker j
+WHERE Education_Lvl = 'Master' AND e.Education_Id = j.Education_Id;
